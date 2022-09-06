18d0b8a652d47e8830db0d5b14b271cf3a618482
moka-a <aud1220a@gmail.com>
2022-09-06 11:54:49 +0900

    bug fix

--- pages/index.vue
+++ pages/index.vue
@@ -8,22 +8,6 @@
     </div>
 
     <div class="fixed inset-0">
-      <div class="flex items-center h-20 px-8 md:px-14 md:h-24 drop-shadow-sm">
-        <Button
-          class="flex items-center space-x-3 cursor-pointer"
-          @click="explain = true"
-        >
-          <div class="text-2xl font-semibold text-white">쿨파스</div>
-
-          <ImageSource
-            class="transform pointer-events-none w-7 h-7 rotate-12"
-            :src="require('~/assets/images/coolcat.jpeg')"
-          />
-        </Button>
-      </div>
-    </div>
-
-    <div class="fixed inset-0">
       <div class="flex items-center justify-center h-full">
         <div class="flex flex-col space-y-2 drop-shadow">
           <div class="text-xl font-semibold text-center text-white">
@@ -42,13 +26,29 @@
       </div>
     </div>
 
+    <div class="fixed inset-x-0 top-0">
+      <div class="flex items-center h-20 px-8 md:px-14 md:h-24 drop-shadow-sm">
+        <Button
+          class="flex items-center space-x-3 cursor-pointer"
+          @click="explain = true"
+        >
+          <div class="text-2xl font-semibold text-white">쿨파스</div>
+
+          <ImageSource
+            class="transform pointer-events-none w-7 h-7 rotate-12"
+            :src="require('~/assets/images/coolcat.jpeg')"
+          />
+        </Button>
+      </div>
+    </div>
+
     <template v-if="explain">
       <div
-        class="fixed inset-0 bg-black bg-opacity-30"
+        class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-30"
         @click="explain = false"
       >
         <ImageSource
-          class="object-contain w-full h-full p-4 transition-opacity transform md:p-48"
+          class="object-contain w-full p-4 transition-opacity transform h-[66%] md:p-0"
           :src="require('~/assets/images/coolpas_explain.jpeg')"
         />
       </div>
